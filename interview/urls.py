f22d44b04abdb53e7c923267a30eea28e918019b
Tuna Bahadır <tunabahadir39@gmail.com>
2026-01-16 02:12:58 +0300

    Python 3.11 gecisi yapildi ve OpenCV eklendi

--- interview/urls.py
+++ interview/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import InterviewListCreateView
+from .views import InterviewListCreateView, AnalyzeFrameView
 
 urlpatterns = [
     path('sessions/', InterviewListCreateView.as_view(), name='interview_sessions'),
+    path('analyze/', AnalyzeFrameView.as_view(), name='analyze_frame'),
 ]
--- interview/views.py
+++ interview/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics, permissions
 from .models import InterviewSession
 from .serializers import InterviewSessionSerializer
+from .ai_processor import InterviewAI
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser
 
 class InterviewListCreateView(generics.ListCreateAPIView):
     
@@ -14,3 +18,19 @@ class InterviewListCreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         
         serializer.save(user=self.request.user)
+
+class AnalyzeFrameView(APIView):
+
+    parser_classes = [MultiPartParser]
+
+    def post(self, request, format=None):
+        if 'image' not in request.data:
+            return Response({"error": "Resim bulunamadı"}, status=400)
+
+        image_file = request.data['image']
+        image_bytes = image_file.read()
+
+        ai_engine = InterviewAI()
+        result = ai_engine.process_frame(image_bytes)
+
+        return Response(result)
