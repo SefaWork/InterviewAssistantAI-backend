74cc6acf370b79116e26a007add58a4633b04578
SefaWork <mehmetsefatoksoy@gmail.com>
2026-05-22 20:17:42 +0300

    Add interview session creation.

--- interview/serializers.py
+++ interview/serializers.py
@@ -5,12 +5,5 @@ from .models import InterviewSession
 class InterviewSessionCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = InterviewSession
-        fields = ('id', 'user', 'created_at')
-        read_only_fields = ('id', 'user', 'created_at')
-
-# DEPRECATED SERIALIZER.
-class InterviewSessionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = InterviewSession
-        fields = ['id', 'user', 'created_at', 'emotion_score', 'eye_contact_score', 'feedback']
-        read_only_fields = ['id', 'user', 'created_at']
+        fields = ['id', 'user']
+        read_only_fields = ['user']
