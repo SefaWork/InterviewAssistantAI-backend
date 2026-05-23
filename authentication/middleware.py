63f3bce9ef924bf0665492802c634b1c2fe2cd03
SefaWork <mehmetsefatoksoy@gmail.com>
2026-05-23 15:49:48 +0300

    Add ticket based auth middleware for Django channels.

--- authentication/middleware.py
+++ authentication/middleware.py
@@ -5,36 +5,33 @@ from django.contrib.auth.models import AnonymousUser
 from rest_framework_simplejwt.tokens import AccessToken
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 from django.contrib.auth import get_user_model
+from django.core.cache import cache
 
 User = get_user_model()
 
 @database_sync_to_async
-def get_user_from_token(token_key):
+def get_user_from_ticket(ticket):
     try:
-        token = AccessToken(token_key)
-        user_id = token["user_id"]
+        user_id = cache.get(f"ws_ticket:{ticket}")
+        cache.delete(f"ws_ticket:{ticket}")
+        if not user_id:
+            return AnonymousUser()
+
         return User.objects.get(id=user_id)
-    except (InvalidToken, TokenError, User.DoesNotExist):
+    except:
         return AnonymousUser()
 
-class JWTChannelAuthMiddleware(BaseMiddleware):
+class TicketChannelMiddleware(BaseMiddleware):
     """This is a middleware class to implement JWT authentication for websockets. TODO: Change or depracate this logic in favor of one-time tickets."""
     async def __call__(self, scope, receive, send):
         # Try query string first: ws://.../?token=<jwt>
         query_string = scope.get("query_string", b"").decode()
         params = parse_qs(query_string)
-        token_key = params.get("token", [None])[0]
-
-        # Fall back to headers: Authorization: Bearer <jwt>
-        if not token_key:
-            headers = dict(scope.get("headers", []))
-            auth_header = headers.get(b"authorization", b"").decode()
-            if auth_header.startswith("Bearer "):
-                token_key = auth_header.split(" ", 1)[1]
+        ticket_key = params.get("ticket", [None])[0]
 
         scope["user"] = (
-            await get_user_from_token(token_key)
-            if token_key
+            await get_user_from_ticket(ticket_key)
+            if ticket_key
             else AnonymousUser()
         )
 
