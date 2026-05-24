4dc1117cd8bb684819deaab4d55a5e2df8b2fdcd
SefaWork <mehmetsefatoksoy@gmail.com>
2026-05-24 16:01:50 +0300

    Add emotion scoring and session create rejection.

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CookieTokenRefreshView, CookieTokenObtainPairView
+from .views import CookieTokenRefreshView, CookieTokenObtainPairView, RegisterView
 
 urlpatterns = [
     #path('register/', RegisterView.as_view(), name='auth_register'),
     path('login/', CookieTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name="register")
 ]
--- interview/models.py
+++ interview/models.py
@@ -5,10 +5,12 @@ class InterviewSession(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='interviews')
     created_at = models.DateTimeField(auto_now_add=True)
 
-    emotion_score = models.FloatField(default=0.0)
-    eye_contact_score = models.FloatField(default=0.0)
-
+    emotion_score_total = models.IntegerField(default=0.0)
+    eye_score_total = models.IntegerField(default=0.0)
+    frame_count = models.IntegerField(default=0.0)
     feedback = models.TextField(blank=True, null=True)
 
+    completed = models.BooleanField(default=False)
+
     def __str__(self):
-        return f"{self.user.email} - Skoru: {self.emotion_score}"
+        return f"{self.user.email} - Score: {self.emotion_score_total / self.frame_count}"
