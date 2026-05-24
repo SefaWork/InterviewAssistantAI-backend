b654976c25db190645c22e3b8bbf6b06d0175e9b
SefaWork <mehmetsefatoksoy@gmail.com>
2026-05-24 22:37:12 +0300

    Fix urls.

--- account/urls.py
+++ account/urls.py
@@ -4,8 +4,8 @@ from .views import SessionListView, SessionDisplayView, ChangeEmailView, ChangeP
 urlpatterns = [
     path('interviews/', SessionListView.as_view(), name="list_interviews"),
     path('interviews/<uuid:pk>/', SessionDisplayView.as_view(), name="display_interview"),
-    path("delete-interview", DeleteSessionView.as_view(), name="delete_interview"),
-    path('change-password', ChangePasswordView.as_view(), name="change_password"),
-    path("change-email", ChangeEmailView.as_view(), name="change_email"),
-    path("delete-account", DeleteAccountView.as_view(), name="delete_account")
+    path('delete-interview/', DeleteSessionView.as_view(), name="delete_interview"),
+    path('change-password/', ChangePasswordView.as_view(), name="change_password"),
+    path('change-email/', ChangeEmailView.as_view(), name="change_email"),
+    path('delete-account/', DeleteAccountView.as_view(), name="delete_account")
 ]
