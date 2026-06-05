2ed6124f78b41d5230123de0de2d72ff4b3771e4
SefaWork <mehmetsefatoksoy@gmail.com>
2026-06-05 19:27:32 +0300

    Track each individual emotion.

--- interview/views.py
+++ interview/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
+from .emotion_weights import EMOTION_SCORE_WEIGHTS
 from .models import OngoingInterviewSession
 
 class InterviewCreateView(APIView):
@@ -17,3 +18,7 @@ class InterviewCreateView(APIView):
         OngoingInterviewSession.objects.create(user=request.user)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+class EmotionWeightsView(APIView):
+
+    def get(self):
+        return Response(EMOTION_SCORE_WEIGHTS, status=status.HTTP_200_OK)
