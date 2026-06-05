4c37b53ac5b5be902f9ac629f274146359478ee8
SefaWork <mehmetsefatoksoy@gmail.com>
2026-06-05 19:30:42 +0300

    Add emotion weights view.

--- interview/urls.py
+++ interview/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import InterviewCreateView
+from .views import InterviewCreateView, EmotionWeightsView
 
 urlpatterns = [
-    path('create/', InterviewCreateView.as_view(), name="create_session")
+    path('create/', InterviewCreateView.as_view(), name="create_session"),
+    path('weights/', EmotionWeightsView.as_view(), name="get_emotion_weights")
 ]
--- interview/views.py
+++ interview/views.py
@@ -19,6 +19,5 @@ class InterviewCreateView(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 class EmotionWeightsView(APIView):
-
     def get(self):
         return Response(EMOTION_SCORE_WEIGHTS, status=status.HTTP_200_OK)
