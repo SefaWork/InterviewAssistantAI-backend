be2b8d3247f165c7de7ad33a9bda448b3aec35f3
Tuna Bahadır <tunabahadir39@gmail.com>
2026-06-06 17:29:42 +0300

    requirements.txt dosyasına mediapipe eklendi, ai_processor null dönüşü güncellendi

--- interview/ai_processor.py
+++ interview/ai_processor.py
@@ -69,7 +69,7 @@ class InterviewAI:
         face_detected = False
         face_count = 0
         eye_contact = False
-        emotion = "Yüz Bulunamadı"
+        emotion = "Unknown"
         confidence = 0.0
 
         mesh_results = self.face_mesh.process(rgb_frame)
