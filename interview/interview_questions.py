369a66c91b442aa05bb4f757d96211c293cbedb6
SefaWork <mehmetsefatoksoy@gmail.com>
2026-06-06 21:19:08 +0300

    Add interview questions file.

---
+++ interview/interview_questions.py
@@ -0,0 +1,22 @@
+interview_questions = {
+    # Usually asked first thing.
+    "early": [
+        "about_self",
+        "why_work",
+    ],
+
+    # Asked in the middle of the interview.
+    "mid": [
+        "strengths",
+        "weaknesses",
+        "challenge",
+        "stress",
+        "pressure"
+    ],
+
+    # Asked near the closing of the session.
+    "late": [
+        "future",
+        "goals",
+    ]
+}
